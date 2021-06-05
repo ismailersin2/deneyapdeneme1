ad802871c4efc888a16546e0d2bae2ba5ce53729
ismailersin <ismail.ersin@yahoo.com>
2021-06-05 13:56:40 +0300

    sorular
    Sorular eklenmiştir

---
+++ Proje1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Github Deneme" << endl;
+    return 0;
+
+    //hsoru 1
+
+
+
+
+
+    //soru 2
+
+
+
+
+
+
+    //soru 3
+
+
+
+
+
+}
